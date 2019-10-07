62d92620b4f28f0f4f76eccb4f54bd21a48bb9e8
Hoàng Anh Trịnh Bùi <56040526+Bongtaiga@users.noreply.github.com>
2019-10-08 01:41:40 +0700

    Add files via upload
    If

--- Main.java
+++ Main.java
@@ -1,70 +1,23 @@
-/*
-package com.company;
-        import javax.print.DocFlavor;
-        import java.math.BigInteger;
-        import java.sql.SQLOutput;
-        import java.time.LocalDate;
-        import java.util.Scanner;
+package sample;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-
-        int i = 1;
-        while (i<=100) {
-            System.out.print( i + " " ); i++;
-        }
-
+        launch(args);
     }
 }
-*/
-
-//Bài 2
-
-        package com.company;
-import javax.print.DocFlavor;
-import java.math.BigInteger;
-import java.sql.SQLOutput;
-import java.time.LocalDate;
-import java.util.Scanner;
-
-public class Main {
-
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-
-        System.out.println("nhap so:");
-        int a = in.nextInt();
-
-        if (a <= 0) {
-            System.out.println("Lỗi");
-        }
-        else if (a >0 ){
-            int tong=0;
-           for (int i=1; i<=a; i++){
-              tong = tong + (tong+1);}
-            System.out.println( tong );
-            }
-        }
-        }
-
-/*
-//Bài 4
-        package com.company;
-import javax.print.DocFlavor;
-import java.math.BigInteger;
-import java.sql.SQLOutput;
-import java.time.LocalDate;
-import java.util.Scanner;
-
-public class Main {
-
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        for (int i = 1; i <= 10; i++) {
-            for (int j = 0; j < 10; j++) {
-                System.out.printf("%d x %d = %d \n", i, j, (i * j));            }
-        }
-    }
-}*/
